f355a6b9bbaeb1ca6a5bc3bcbcd8f50ab45b8e8a
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-06-13 21:58:02 +0200

    adding extra comments

--- selection_Sort.py
+++ selection_Sort.py
@@ -1,10 +1,10 @@
-def selection_sort(array):
-    for i in range(0, len(array) - 1):
+def selection_sort(array):      # defining function
+    for i in range(0, len(array) - 1):      # outer loop will iterate throw the array length - 1
         min_ind = i
-        for j in range(i + 1, len(array)):
-            if array[j] < array[min_ind]:
+        for j in range(i + 1, len(array)):  # inner loop will iterate one index front
+            if array[j] < array[min_ind]:   # condition will compare
                 min_ind = j
-        array[i], array[min_ind] = array[min_ind], array[i]
+        array[i], array[min_ind] = array[min_ind], array[i]     # swap
 
 
 array = [5, 6, 3, 2, 7, 6, 5, 8, 9, 4]
